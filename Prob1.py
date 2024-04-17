42788f5cde3ecb08a02385520eb3ba79a0cf0f14
Jed Rembold <jedediah.r@gmail.com>
2024-04-17 01:44:44 -0700

    Add new Q1 and update pdf

---
+++ Prob1.py
@@ -0,0 +1,22 @@
+
+"""
+Creating an Item class for use in a store or shop
+"""
+
+class Item:
+    # Your code here!
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ball = Item("Red ball", 10, 2.50)
+    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
+    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
+    ball.restock(8)
+    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
+    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
+
