d3d4e7e549e7bee955178b28606c237d1726d869
Jed Rembold <jedediah.r@gmail.com>
2025-11-18 22:57:04 -0800

    Switch to Infinite Adventure content

--- Prob1.py
+++ Prob1.py
@@ -1,22 +1,45 @@
 
 """
-Creating an Item class for use in a store or shop
+Reading in and working with the Space Missions JSON data
 """
 
-class Item:
-    # Your code here!
+import json
 
 
+def load_data():
+    """Loads in the space_missions.json data and returns the corresponding dictionary
+    
+    Inputs:
+        None
+    Outputs:
+        (dict) - The JSON dictionary
+    """
 
 
+def eva_count(data):
+    """Computes how many different crew members have EVA certification and have served
+    on missions that begin with an A.
+
+    Inputs:
+        data (dict) - The space missions dictionary
+    Outputs:
+        (int) - The count of the number of unique crew members
+    """
+
+
+def power_missions(data):
+    """Filters to find the unique mission names where Navigation equipment, on average,
+    was drawing more than 100 W.
+
+    Inputs:
+        data (dict) - The space missions dictionary
+    Outputs:
+        (set of str) - The unique set of mission names
+    """
 
 
 
 if __name__ == '__main__':
-    ball = Item("Red ball", 10, 2.50)
-    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
-    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
-    ball.restock(8)
-    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
-    print(f"Trying to purchase 6 balls. Charged: {ball.purchase(6)}")
-
+    data = load_data()
+    # print(eva_count(data))
+    # print(power_missions(data))
---
+++ Prob2.py
@@ -0,0 +1,24 @@
+
+"""
+Makes a simple POST request and interprets the results
+"""
+
+import json
+import requests
+
+API_URL = "https://section12api-production.up.railway.app/generate"
+
+
+# Construct your payload with the needed keys
+
+
+# Make the post request, providing the payload
+
+
+# The the returned status
+
+
+# Extract the content key, and convert back to a dictionary
+
+
+# Extract and print the "fun_fact" key from the content dictionary
